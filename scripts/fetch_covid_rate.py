af5ca5ce015ed4a0b3fd6a9185da2ab2d801eee8
JRInge <JRInge@users.noreply.github.com>
2021-02-20 17:21:00 +0000

    Add COVID-19 sensor functionality

---
+++ scripts/fetch_covid_rate.py
@@ -0,0 +1,28 @@
+#!/srv/homeassistant/bin/python3
+#
+# Scrape local case rate from data.gov.uk
+# Returns a single floating point number in cases/100k.
+#
+
+from argparse import ArgumentParser, Namespace
+from urllib.parse import quote_plus
+from urllib.request import urlopen
+from bs4 import BeautifulSoup      # type: ignore
+from bs4.element import ResultSet  # type: ignore
+
+url: str = "https://coronavirus.data.gov.uk/search?postcode="
+
+
+def get_args() -> Namespace:
+    parser: ArgumentParser = ArgumentParser(description=(
+      "Fetch local COVID rate information for given UK postcode."))
+    parser.add_argument('postcode')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = get_args()
+    html = urlopen(url + quote_plus(args.postcode))
+    soup: BeautifulSoup = BeautifulSoup(html, 'lxml')
+    tags: ResultSet = soup.find_all('strong')
+    print(tags[0].text)
